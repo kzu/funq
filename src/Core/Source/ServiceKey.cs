4856d4d84c53a1ffe4c48140e73db2f2667e4d31
dcazzulino <devnull@localhost>
2009-04-13 03:46:14 +0000

    Sweezed about 7% more performance by caching the hashcode in the key (we always generate it at least once, so we eagerly do this in the ctor. now)

--- src/Core/Source/ServiceKey.cs
+++ src/Core/Source/ServiceKey.cs
@@ -4,10 +4,16 @@ namespace Funq
 {
 	internal sealed class ServiceKey
 	{
+		int hash;
+
 		public ServiceKey(Type factoryType, string serviceName)
 		{
 			FactoryType = factoryType;
 			Name = serviceName;
+
+			hash = factoryType.GetHashCode();
+			if (serviceName != null)
+				hash ^= serviceName.GetHashCode();
 		}
 
 		public Type FactoryType;
@@ -28,22 +34,16 @@ namespace Funq
 		public static bool Equals(ServiceKey obj1, ServiceKey obj2)
 		{
 			if (Object.Equals(null, obj1) ||
-				Object.Equals(null, obj2) ||
-				obj1.GetType() != obj2.GetType())
+				Object.Equals(null, obj2))
 				return false;
 
 			if (Object.ReferenceEquals(obj1, obj2)) return true;
 
-			return obj1.FactoryType == obj2.FactoryType && 
-				obj1.Name == obj2.Name;
+			return obj1.hash == obj2.hash;
 		}
 
 		public override int GetHashCode()
 		{
-			int hash = FactoryType.GetHashCode();
-			if (Name != null)
-				hash ^= Name.GetHashCode();
-
 			return hash;
 		}
 
