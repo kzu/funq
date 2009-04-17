b90e42d551abd6448ad07eb441b6eb26e9bea116
dcazzulino <devnull@localhost>
2009-04-17 01:45:59 +0000

    Removed dependency on conventions funqlet

--- src/Samples/Performance/Source/FunqUseCase.cs
+++ src/Samples/Performance/Source/FunqUseCase.cs
@@ -8,27 +8,6 @@ using System.Reflection;
 
 namespace Performance
 {
-	public partial class DomainFunqlet : IConventionFunqlet
-	{
-		public Convention GetConvention()
-		{
-			return new Convention(ConventionKind.Services,
-				from t in Assembly.GetExecutingAssembly().GetExportedTypes()
-				where t.Namespace == "Domain" && t.IsClass && !t.IsAbstract
-				select t)
-			{
-				Reuse = ReuseScope.None
-			};
-		}
-
-		public void Configure(Container container)
-		{
-			DoConfigure(container);
-		}
-
-		partial void DoConfigure(Container container);
-	}
-
 	[System.ComponentModel.Description("Funq")]
 	public class FunqUseCase : UseCase
 	{
@@ -38,34 +17,30 @@ namespace Performance
 		{
 			container = new Container { DefaultReuse = ReuseScope.None };
 
-			// Remove the need for all the following code 
-			// by implementing a funqlet
-			new DomainFunqlet().Configure(container);
+			container.Register<IWebApp>(
+				c => new WebApp(
+					c.Resolve<IAuthenticator>(),
+					c.Resolve<IStockQuote>()));
 
-			//container.Register<IWebApp>(
-			//   c => new WebApp(
-			//      c.Resolve<IAuthenticator>(),
-			//      c.Resolve<IStockQuote>()));
+			container.Register<IAuthenticator>(
+				c => new Authenticator(
+					c.Resolve<ILogger>(),
+					c.Resolve<IErrorHandler>(),
+					c.Resolve<IDatabase>()));
 
-			//container.Register<IAuthenticator>(
-			//   c => new Authenticator(
-			//      c.Resolve<ILogger>(),
-			//      c.Resolve<IErrorHandler>(),
-			//      c.Resolve<IDatabase>()));
+			container.Register<IStockQuote>(
+				c => new StockQuote(
+					c.Resolve<ILogger>(),
+					c.Resolve<IErrorHandler>(),
+					c.Resolve<IDatabase>()));
 
-			//container.Register<IStockQuote>(
-			//   c => new StockQuote(
-			//      c.Resolve<ILogger>(),
-			//      c.Resolve<IErrorHandler>(),
-			//      c.Resolve<IDatabase>()));
+			container.Register<IDatabase>(
+				c => new Database(
+					c.Resolve<ILogger>(),
+					c.Resolve<IErrorHandler>()));
 
-			//container.Register<IDatabase>(
-			//   c => new Database(
-			//      c.Resolve<ILogger>(),
-			//      c.Resolve<IErrorHandler>()));
-
-			//container.Register<IErrorHandler>(
-			//   c => new ErrorHandler(c.Resolve<ILogger>()));
+			container.Register<IErrorHandler>(
+				c => new ErrorHandler(c.Resolve<ILogger>()));
 
 			container.Register<ILogger>(new Logger());
 		}
