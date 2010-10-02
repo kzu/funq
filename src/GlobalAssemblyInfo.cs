b8db60d904bc90e99458eac893c541b625c3195e
dcazzulino <devnull@localhost>
2010-10-01 19:38:46 -0700

    Updated to latest SL builds.

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Clarius Consulting")]
+[assembly: AssemblyProduct("Funq Dependency Injection Framework")]
+[assembly: AssemblyCopyright("Copyright (C) Microsoft Corporation")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Silverlight/UnitTests/App.xaml.cs
+++ src/Silverlight/UnitTests/App.xaml.cs
@@ -1,46 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Silverlight.Testing;
-using System.Diagnostics;
-using System.Windows.Browser;
 
-namespace Funq.Silverlight.Tests
+namespace UnitTests
 {
 	public partial class App : Application
 	{
+
 		public App()
 		{
-			this.Startup += this.OnApplicationStartup;
-			this.Exit += this.OnApplicationExit;
-			this.UnhandledException += this.OnApplicationUnhandledException;
+			this.Startup += this.Application_Startup;
+			this.Exit += this.Application_Exit;
+			this.UnhandledException += this.Application_UnhandledException;
 
-			this.InitializeComponent();
+			InitializeComponent();
 		}
 
-		private void OnApplicationStartup(object sender, StartupEventArgs e)
+		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			this.RootVisual = UnitTestSystem.CreateTestPage();
+			RootVisual = UnitTestSystem.CreateTestPage();
 		}
 
-		private void OnApplicationExit(object sender, EventArgs e)
+		private void Application_Exit(object sender, EventArgs e)
 		{
-		}
 
-		private void OnApplicationUnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+		}
+		private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
 		{
 			// If the app is running outside of the debugger then report the exception using
 			// the browser's exception mechanism. On IE this will display it a yellow alert 
 			// icon in the status bar and Firefox will display a script error.
-			if (!Debugger.IsAttached)
+			if (!System.Diagnostics.Debugger.IsAttached)
 			{
 
 				// NOTE: This will allow the application to continue running after an exception has been thrown
@@ -48,21 +38,17 @@ namespace Funq.Silverlight.Tests
 				// For production applications this error handling should be replaced with something that will 
 				// report the error to the website and stop the application.
 				e.Handled = true;
-				Deployment.Current.Dispatcher.BeginInvoke(
-					(Action<ApplicationUnhandledExceptionEventArgs>)this.ReportErrorToDOM,
-					e);
+				Deployment.Current.Dispatcher.BeginInvoke(delegate { ReportErrorToDOM(e); });
 			}
 		}
-
 		private void ReportErrorToDOM(ApplicationUnhandledExceptionEventArgs e)
 		{
 			try
 			{
-				var errorMsg = e.ExceptionObject.Message + e.ExceptionObject.StackTrace
-					.Replace('"', '\'')
-					.Replace("\r\n", @"\n");
+				string errorMsg = e.ExceptionObject.Message + e.ExceptionObject.StackTrace;
+				errorMsg = errorMsg.Replace('"', '\'').Replace("\r\n", @"\n");
 
-				HtmlPage.Window.Eval("throw new Error(\"Unhandled Error in Silverlight 2 Application " + errorMsg + "\");");
+				System.Windows.Browser.HtmlPage.Window.Eval("throw new Error(\"Unhandled Error in Silverlight Application " + errorMsg + "\");");
 			}
 			catch (Exception)
 			{
