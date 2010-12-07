20790e95f9b0f466af0e6187156f931e78ef1e6d
dcazzulino <devnull@localhost>
2010-12-07 15:23:16 -0300

    work in progress creating nuget for funq and automatic build-time dependency generation.

---
+++ src/Core/Funq.Build/NuPack/Tools/Init.ps1
@@ -0,0 +1,14 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$global:funqletPath = $installPath
+$global:funqletTools = $toolsPath
+Write-Host installPath=$global:funqletPath, toolsPath=$global:funqletTools
+
+function global:Add-Funqlet {
+    [CmdletBinding()]
+    param ([string]$project)
+    Process {
+        # 
+		Write-Host Added to $project
+    }
+}
---
+++ src/VisualStudio/Funq.VisualStudio/Templates/Projects/DesktopCompositionFunqlet/Funqlet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Funq;
+
+namespace $safeprojectname$
+{
+	public partial class Funqlet : IFunqlet
+	{
+		partial void DoConfigure(Container container)
+		{
+			// Provide custom registrations here if needed.
+		}
+	}
+}
